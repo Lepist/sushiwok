4aac56f6249f9a0afb13b822bafa581d76bcd397
Lepist <assault912@mail.ru>
2022-03-17 14:41:15 +0300

    sendmess,getmess

--- index.js
+++ index.js
@@ -1,48 +1,27 @@
 /*
-https://wwebjs.dev/ - библиотека(API) для whatsApp бота, использует Puppeteer(эмулятор Chrome)
+https://wwebjs.dev/ - библиотека(API) для whatsApp бота; использует Puppeteer(эмулятор Chrome)
 */
-const fs = require('fs');
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
-const SESSION_FILE_PATH = './session.json';
-
 // Загружаем данные сессии, если они были сохранены ранее
-let client;
-let sessionData;
-if(fs.existsSync(SESSION_FILE_PATH)) {
-    sessionData = require(SESSION_FILE_PATH);
-    // Создаем клиент с сохраненной сессией(если такая есть)
-    client = new Client({
-        authStrategy: new LocalAuth({
-            session: sessionData
-        })
-    });
-}else{
-    client = new Client();
-}
+const client =  new Client({
+    authStrategy: new LocalAuth()
+});
 
 client.on('qr', qr => {
     qrcode.generate(qr, {small: true});
 });
 
-// Сохраняем значения сессии в файл после успешной авторизации
-client.on('authenticated', (session) => {
-    sessionData = session;
-    // Записываем данные сессии в файл
-    /* (node:17224) UnhandledPromiseRejectionWarning: TypeError [ERR_INVALID_ARG_TYPE]: The "data" argument must be of type string or an instance of Buffer, TypedArray, or DataView. Received undefined - разберусь */    
-    // fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), {encoding: 'utf8'}, err => {
-    //     if(err){
-    //         console.error(`[ERROR] Ошибка записи файла ${SESSION_FILE_PATH}: ${err}`);
-    //     }
-    // });
-    console.log(session);
-    console.log(JSON.stringify(session));
+client.on('authenticated', () => {
     console.log(`[LOG] Succesfully authenticated.`);
 });
 
 client.on('ready', () => {
     console.log('[LOG] Client is ready!');
+    let text = "Hello";
+    let chatId = "79266481762" + "@c.us"; // chatid лички юзера с номером (т.е. пишем напрямую)
+    client.sendMessage(chatId, text); // отправляем сообщение
 });
 
 client.initialize();
@@ -50,5 +29,11 @@ client.initialize();
 client.on('message', async msg => {
     const chat = await msg.getChat();
     const contact = await msg.getContact();
-    await chat.sendMessage(`Привет @${contact.id.user}`);
+    console.log(msg.body); // выводим в консоль сообщение, которое пришло 
 });
+
+
+/* 
+    Пока что не совсем понятно как создать событие-слушателя для конкретного юзера(почитаю документацию поподробнее), как вариант создать копию client.on('message') для конкретного пользователя на время переписки, а после удалять и оставлять только основной слушатель всех сообщений. Либо дописать новый ивент в библиотеку(в этом случае придется каждый раз дописывать его при обновлении библиотеки, не совсем удобно)
+
+*/
